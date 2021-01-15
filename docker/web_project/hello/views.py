c569d3a8126fd69b218edfcecf17a576e1980813
mulondumuema <mulondumuema@gmail.com>
2021-01-15 20:53:55 +0300

    added the feature

--- docker/web_project/hello/views.py
+++ docker/web_project/hello/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to SCA Cloud School Application")
+    return HttpResponse("Welcome to SCA Cloud School Application , this is my first assessment")
